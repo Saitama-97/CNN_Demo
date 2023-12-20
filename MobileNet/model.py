cdc8b2beff6279f8d17ac01260781f50626d198a
Saitama <2822863494@qq.com>
2023-12-20 17:46:15 +0800

    MobileNet Pre

---
+++ MobileNet/model.py
@@ -0,0 +1,16 @@
+# _*_ coding: utf-8 _*_
+
+"""
+  @Time    : 2023.12.20 17:39
+  @File    : model.py
+  @Project : cnn_demo
+  @Author  : Saitama
+  @IDE     : PyCharm
+  @Desc    : GoogleNet 手搓
+            模型定义
+"""
+
+import torch
+import torch.nn as nn
+
+
